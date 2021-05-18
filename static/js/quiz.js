0ac7ec0a240d93b5fe5efc34fb8fdd6c3202361c
michael <mparkcode@gmail.com>
2021-05-18 19:46:09 +0900

    continuing quiz functionality

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,2 +1,32 @@
 console.log(verbs)
-console.log(parent_verbs)
+
+const quiz = (verbs) => {
+    let i = 0
+    verb = verbs[i]
+    document.getElementById('verbToGuess').textContent = verb.english
+    let answers = generate_answers(verb)
+}
+
+const generate_answers = verb => {
+    let answers = [verb.korean]
+    while (answers.length < 4) {
+        let answer = parent_verbs[Math.floor(Math.random() * parent_verbs.length)]
+        if(!answers.includes(answer.korean)){
+            answers.push(answer.korean)
+        }
+    }
+    answers = shuffleArray(answers)
+    console.log(answers)
+}
+
+const shuffleArray = arr => {
+    let newArr = []
+    while (newArr.length < 4 ){
+        let idx = Math.floor(Math.random() * arr.length)
+        newArr.push(arr[idx])
+        arr.splice(idx, 1)
+    }
+    return newArr
+}
+
+quiz(verbs)
